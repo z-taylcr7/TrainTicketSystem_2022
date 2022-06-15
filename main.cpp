a53849659ae49dd3eebee47760bcc4ce59ceb05d
zhangchengkai <31203538+zhangchengkai@users.noreply.github.com>
2022-06-16 02:04:09 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include "cmd.hpp"
 #include <iostream>
 #include <fstream>
-#include "ErrorMessage.h"
 
 
 //std::ifstream in("../data/basic_1/1.in");
@@ -11,18 +10,10 @@
 
 int main()
 {
+//	printf("%d\n",sizeof(HashMap) ;
 //    freopen("1.in","r",stdin);
 //    freopen("1.ans","w",stdout);
-	const bool is_online=1; 
     cmd ticketLibrary;
-    if(is_online)
-    {
-        std::ios::sync_with_stdio(false);
-        try {ticketLibrary.run(std::cin,std::cout);}
-        catch(error x)
-        {
-            std::cout<<x.what()<<std::endl;
-            return 0;
-        }
-    }
+    std::ios::sync_with_stdio(false);
+    ticketLibrary.run(std::cin,std::cout);
 }
